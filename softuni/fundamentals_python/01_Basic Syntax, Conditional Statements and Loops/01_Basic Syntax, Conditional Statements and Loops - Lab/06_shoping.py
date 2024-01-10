fc936609f0f74c033daaed2887f2384086471431
Nenogzar <stoyan.naidenov@gmail.com>
2024-01-10 21:40:47 +0200

    update

--- softuni/fundamentals_python/01_Basic Syntax, Conditional Statements and Loops/01_Basic Syntax, Conditional Statements and Loops - Lab/06_shoping.py
+++ softuni/fundamentals_python/01_Basic Syntax, Conditional Statements and Loops/01_Basic Syntax, Conditional Statements and Loops - Lab/06_shoping.py
@@ -6,7 +6,7 @@ while True:
     if command == "End":
         print("You bought everything needed.")
         break
-    current_price = float(command)
+    current_price = int(command)
     budget -= current_price
     if budget <= 0:
         print("You went in overdraft!")
