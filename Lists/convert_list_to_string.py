28c4382c5c4bde6f8b9a30eaaabcfa0e73dd123c
Nenogzar <stoyan.naidenov@gmail.com>
2024-01-14 01:00:26 +0200

    Update

---
+++ Lists/convert_list_to_string.py
@@ -0,0 +1,5 @@
+# Convert List to String
+
+list = ["Welcome", 2, "Peshtera"]
+string = ' '.join([str(elem) for elem in list])
+print(string)
---
+++ time/calculate_per_time.py
@@ -0,0 +1,53 @@
+import random
+import time
+
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+best_time = 500000000000.00
+count_game = 0
+
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
+
+
+
+
+wrong = 0
+input("Press enter to start!")
+print("----------------------")
+
+start_time = time.time()
+while True:
+    count_game += 1
+    for i in range(TOTAL_PROBLEMS):
+        expr, answer = generate_problem()
+        while True:
+            guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+            if guess == str(answer):
+                break
+            wrong += 1
+
+    end_time = time.time()
+    total_time = round(end_time - start_time, 2)
+    if best_time > total_time:
+        best_time = total_time
+
+    print("----------------------")
+    print("Nice work! You finished in", total_time, "seconds!")
+    print(f"{count_game = }")
+    print(f"{best_time = }")
+
+    repeat = input("Искате ли да повторите (Y/N)? ")
+    if repeat.upper() == "N":
+        print("Благодаря!")
+        break
