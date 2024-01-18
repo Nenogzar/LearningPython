6c9ceb12b3ae6e23c1c356c90e25a275c10f58db
Nenogzar <stoyan.naidenov@gmail.com>
2024-01-18 08:21:11 +0200

    calendar

---
+++ Other/calendar/main.py
@@ -0,0 +1,15 @@
+# importing the calendar module
+import calendar
+# initializing the year
+year = int(input('input year:'))
+# printing the calendar
+print(calendar.calendar(year))
+
+
+
+""" month """
+# initializing the year and month
+year = int(input('input year:'))
+month = int(input('input month:'))
+# printing the calendar
+print(calendar.month(year, month))
