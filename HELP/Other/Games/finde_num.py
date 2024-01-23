22529f1be295e581f5a6ac37c6a2bf16bfe93c47
Nenogzar <stoyan.naidenov@gmail.com>
2024-01-23 22:24:25 +0200

    new tasks

--- HELP/Other/Games/finde_num.py
+++ HELP/Other/Games/finde_num.py
@@ -3,22 +3,22 @@ import random
 num_ran = random.randint(1, 100)
 print(num_ran)
 
-guess_count = 0  # Променлива за брояч на опитите
+guess_count = 0
 
 while True:
-    guess_count += 1  # Увеличаваме брояча с 1 при всяко ново опитване
-    num1 = int(input("кое е числото? "))
+    guess_count += 1
+    num1 = int(input("what is the number? "))
 
     if num1 < num_ran:
-        print("По-нагоре ")
+        print("Higher ")
         continue
     elif num1 > num_ran:
-        print("По-надолу")
+        print("Below")
         continue
     else:
-        print(f"Браво позна числото след {guess_count} опита!")
+        print(f"Well done guess the number after {guess_count} tries!")
 
-    repeat = input("Искате ли да повторите (Y/N)? ")
+    repeat = input("Do you want to replay the game? (Y/N)? ")
     if repeat.upper() == "N":
-        print("Благодаря!")
+        print("thank you!")
         break
