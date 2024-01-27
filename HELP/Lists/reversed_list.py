01b32b78d8bac3cedf5bead827bec1da07371739
Nenogzar <stoyan.naidenov@gmail.com>
2024-01-27 19:12:57 +0200

    move list to SoftUni

--- HELP/Lists/reversed_list.py
+++
@@ -1,8 +0,0 @@
-code = input("code: ")
-code = code.split()
-message = []
-for i in range(len(code)):
-  if code[len(code)-1-i][0].isupper():
-     message.append(code[len(code)-1-i])
-message = " ".join(message)
-print ("says:",message.lower())
